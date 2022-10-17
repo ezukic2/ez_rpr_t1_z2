fb1d8909331456371ce484a5725f9f4eb49f0840
Emin <ezukic2@etf.unsa.ba>
2022-10-17 14:30:53 +0200

    Initial commit

---
+++ src/DrugiZadatak.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DrugiZadatak {
+    public static int SumaCifara (int x){
+        int sumaCifara=0;
+        while(x!=0){
+            sumaCifara = sumaCifara += x%10;
+            x = x/= 10;
+        }
+        return sumaCifara;
+    }
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Unesite n");
+        Scanner ulaz = new Scanner(System.in);
+        n = ulaz.nextInt();
+        for (int i=1; i<=n; i++) {
+            if(i % SumaCifara(i) == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
